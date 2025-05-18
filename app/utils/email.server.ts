fcf539e10e1d18ba5dfc9b9cf9dbe010ba831b35
Vitalie Andries <vitalie.andies@gmail.com>
2025-05-18 15:58:36 +0300

    chore: Use environment variable for email sender domain

--- app/utils/email.server.ts
+++ app/utils/email.server.ts
@@ -31,7 +31,7 @@ export async function sendEmail({
 	| { html: string; text: string; react?: never }
 	| { react: ReactElement; html?: never; text?: never }
 )) {
-	const from = 'hello@epicstack.dev'
+	const from = `hello@${process.env.SASS_DOMAIN}`;
 
 	const email = {
 		from,

2d4cbf41efad8974b075241d22f65c6f7d3a1e91
Vitalie Andries <vitalie.andies@gmail.com>
2025-07-11 13:24:54 +0300

    chore: update .prettier.mjs file

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+import usernamePrettierConfig from "@epic-web/config/prettier";
+
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+	...usernamePrettierConfig,
+	tabWidth: 4,
+	width: 120,
+	printWidth: 120,
+};
+
+export default config;
